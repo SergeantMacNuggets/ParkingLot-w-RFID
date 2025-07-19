2759764c103e26a1460e25560782a8ec6f5e4740
Deluxo <110329381+SergeantMacNuggets@users.noreply.github.com>
2025-07-19 10:25:57 +0800

    GUI Design Probably Done

--- src/main/java/GUI/ParkingWindow.java
+++ src/main/java/GUI/ParkingWindow.java
@@ -4,11 +4,15 @@ package GUI;
 import BackEnd.ParkingLot;
 import net.miginfocom.swing.MigLayout;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.Objects;
 
 interface LocalColor {
     Color inputColor = new Color(52,87,52);
@@ -25,7 +29,8 @@ public class ParkingWindow extends JFrame {
     private JRadioButton editRoad;
     private JRadioButton editExit;
     private JRadioButton editEnter;
-
+    private JTextField rfidInput;
+    private JTextField RFIDRegisterInput;
 
     public void start() {
         this.parkingLot = new ParkingLot(10,10);
@@ -33,6 +38,8 @@ public class ParkingWindow extends JFrame {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setPreferredSize(new Dimension(1200, 800));
         this.setLayout(new BorderLayout());
+        this.setTitle("Parking Lot System");
+        this.setJMenuBar(new MenuBar());
         south = southPanel();
 
         this.add(northPanel(),BorderLayout.NORTH);
@@ -74,25 +81,62 @@ public class ParkingWindow extends JFrame {
     private JPanel northPanel() {
         JPanel p = new JPanel();
 
+        JPanel left = new JPanel();
+        JPanel right = new JPanel();
+        right.setBorder(BorderFactory.createEmptyBorder(20,0,20,40));
         p.setPreferredSize(new Dimension(0, 100));
 
 
-        p.setLayout(new MigLayout());
-        p.add(new JButton("New") {{this.setPreferredSize(new Dimension(150,70));
+        p.setLayout(new BorderLayout());
+        left.add(new JButton("New") {{this.setPreferredSize(new Dimension(150,70));
                                     this.setBackground(LocalColor.buttonColor);
                                     this.setFont(new Font("Ariel", Font.BOLD,24));}});
-        p.add(new JButton("Open"){{this.setPreferredSize(new Dimension(150,70));
+        left.add(new JButton("Open"){{this.setPreferredSize(new Dimension(150,70));
                                     this.setBackground(LocalColor.buttonColor);
                                     this.setFont(new Font("Ariel", Font.BOLD,24));}});
-
+        right.add(new JLabel("Parking Lot System") {{this.setFont(new Font("Ariel", Font.BOLD, 32));
+                                                    this.setForeground(LocalColor.buttonColor);}});
+        p.add(left,BorderLayout.WEST);
+        p.add(right,BorderLayout.EAST);
         return p;
     }
 
     private JPanel leftPanel() {
-        JPanel p = new JPanel();
-        p.setLayout(new MigLayout());
+        JPanel p = new JPanel(new GridLayout(2,1));
+        try {
+            BufferedImage logo = ImageIO.read(Objects.requireNonNull(this.getClass().getResourceAsStream("/rfid.png" )));
 
-        p.setPreferredSize(new Dimension(200,0));
+            JPanel top = new JPanel(new MigLayout());
+            JPanel bottom = new JPanel(new MigLayout());
+
+            rfidInput = new JTextField();
+            RFIDRegisterInput = new JTextField();
+            rfidInput.setPreferredSize(new Dimension(0, 40));
+            RFIDRegisterInput.setPreferredSize(new Dimension(0,40));
+            rfidInput.addActionListener(e->{
+                        System.out.println(rfidInput.getText());
+                        rfidInput.setText("");
+                    }
+            );
+
+            p.setPreferredSize(new Dimension(200,0));
+
+            top.add(new JLabel("Enter RFID Number: ") {{this.setFont(new Font("Ariel", Font.BOLD, 16));}}, "wrap");
+            top.add(rfidInput, "width 95%");
+            top.add(new JLabel(),"wrap 50");
+            top.add(new JLabel("Register an RFID Number: ") {{this.setFont(new Font("Ariel", Font.BOLD, 15));}}, "wrap");
+            top.add(RFIDRegisterInput, "width 95%");
+            bottom.add(new JLabel(new ImageIcon(logo)), "gapleft 15%");
+            bottom.add(new JLabel(),"wrap 50");
+            bottom.add(new JLabel("Connected!") {{this.setFont(new Font("Ariel", Font.BOLD, 22));
+                                                    this.setForeground(Color.yellow);}}, "gapleft 15%");
+
+            p.add(top);
+            p.add(bottom);
+
+        } catch (IOException e) {
+            System.out.println("Image Not Found");
+        }
 
         return p;
     }
@@ -166,5 +210,17 @@ public class ParkingWindow extends JFrame {
         p.setBackground(LocalColor.inputColor); // Green r:52 g:87 b:52   Red r:197 g:28 b:44
         p.setPreferredSize(new Dimension(0,22));
         return p;
+    }
+}
+
+
+class MenuBar extends JMenuBar {
+
+    MenuBar() {
+        JMenu[] menu = {new JMenu("File"), new JMenu("View"), new JMenu("Help")};
+
+        for(JMenu m: menu) {
+            this.add(m);
+        }
     }
 }
